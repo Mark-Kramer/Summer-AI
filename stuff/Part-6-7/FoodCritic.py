65470317c74f32c2bb16b6922b1994cb0e5a5e27
Mark-Kramer <mak@math.bu.edu>
2020-08-10 14:31:16 -0400

    update food critic

---
+++ stuff/Part-6-7/FoodCritic.py
@@ -0,0 +1,11 @@
+#FoodCritic.py
+
+def food_critic(ice_cream, ketchup, weight_ice_cream, weight_ketchup):
+    # Define the perceptron calculation
+    x = [???]
+    # Apply the threshold,
+    if [???]
+        output = 'Yum!'
+    else:
+        output = 'Yuck!'
+    return output
